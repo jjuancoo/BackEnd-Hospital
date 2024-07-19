44598f1cbd21df6100f152be90d3a532c12a1bdf
jjuancoo <161867767+jjuancoo@users.noreply.github.com>
2024-07-19 14:04:40 -0600

    Update db.py
    Creando la actualizacion de la base de datos remota

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_OfGxS5vHG5BBN464rsR@mysql-326cdfe2-hospitalidgs.d.aivencloud.com:13910/test"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
