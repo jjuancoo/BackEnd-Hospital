d663d42351788cabf2b7ba2affc9c1e1780e7f2f
Juanco <210373@utxicotepec.edu.mx>
2024-08-09 13:21:13 -0600

    Crea el schema para estudios

---
+++ schemas/estudios.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from pydantic import BaseModel
+from datetime import datetime
+from decimal import Decimal
+
+class EstudiosBase(BaseModel):
+    Tipo: str
+    Nivel_Urgencia: str
+    Solicitud_ID: int
+    Consumibles_ID: Optional[int] = None
+    Estatus: str
+    Total_Costo: Decimal
+    Dirigido_A: Optional[str] = None
+    Observaciones: Optional[str] = None
+    Fecha_Registro: datetime
+    Fecha_Actualizacion: Optional[datetime] = None
+
+class EstudiosCreate(EstudiosBase):
+    pass
+
+class EstudiosUpdate(EstudiosBase):
+    pass
+
+class Estudios(EstudiosBase):
+    ID: int
+
+    class Config:
+        orm_mode = True
+
